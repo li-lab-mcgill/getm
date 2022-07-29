6e4122f012fc4c99fdf87c23d8cca3c28fb3b2af
Yueningwang <yw246@uw.edu>
2022-07-29 16:25:34 -0400

    .

--- subjob/run_metm.sh
+++ subjob/run_metm.sh
@@ -1,7 +1,3 @@
 
-python main_multi_etm_sep.py --bow_norm=0 --epochs=10 --lr=0.01 --batch_size=100 --save_path="acute2chronic_results/results_m579c322_topic${topic_num}"\
- --vocab_size1=579 --vocab_size2=322 --data_path="Acute2Chronic/drug579_cond322" --num_topics=${topic_num} --rho_size=128 --emb_size=128 --t_hidden_size=64 --enc_drop=0.0 \
- --train_embeddings1=0 --embedding1="drug_emb.npy" --train_embeddings2=0 --embedding2="code_emb.npy" --rho_fixed1=1 --rho_fixed2=1\
-  --lstm_hidden_size=10 --e2e=0 --num_classes=8 --pred_nlayer=1 --predcoef=2
-
+python main_multi_etm_sep.py --save_path="results/results_m802c443_topic128" --data_path="Unsupervised/drug802_cond443"
 
