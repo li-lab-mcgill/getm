02b0d72a835106320e462095ec9fb76f0ffa9824
Yueningwang <yw246@uw.edu>
2022-11-13 22:54:56 -0500

    bugfixed

--- subjob/run_metm.sh
+++ subjob/run_metm.sh
@@ -1,4 +1,5 @@
 
-python main_multi_etm_sep.py --save_path="results/results_m802c443_topic128" --data_path="Unsupervised/drug802_cond443":q
+python main_multi_etm_sep.py --save_path="Simulation_data/results" --data_path="Simulation_data/input" --vocab_size1 28 \
+--vocab_size2 34 --embedding1 cond_emb.npy --embedding2 med_emb.npy --num_topics 5 --lr 0.01 --batch_size 100
 
 
