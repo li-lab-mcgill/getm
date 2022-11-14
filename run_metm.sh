80fa857b2a2dad38491e05dc9f4d345ed63ed649
Yueningwang <yw246@uw.edu>
2022-11-14 18:22:21 -0500

    Jupyter notebook and simulated data added

--- run_metm.sh
+++ run_metm.sh
@@ -1,5 +1,5 @@
 
 python main_multi_etm_sep.py --save_path="Simulation_data/results" --data_path="Simulation_data/input" --vocab_size1 30 \
---vocab_size2 37 --embedding1 med_emb.npy --embedding2 cond_emb.npy --num_topics 30 --lr 0.01 --batch_size 100
+--vocab_size2 37 --embedding1 med_emb.npy --embedding2 cond_emb.npy --num_topics 5 --lr 0.01 --batch_size 100
 
 
